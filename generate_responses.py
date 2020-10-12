15cb75c1f7a74fe3f976808b37dc02511221b64b
Alexandra DeLucia <aadelucia@jhu.edu>
2020-10-12 14:09:11 -0400

    added response generation code

--- generate_responses.py
+++ generate_responses.py
@@ -24,26 +24,10 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 logging.basicConfig(level=logging.INFO)
 
 
-def batchify_old(sents, bsz):
-    """Lisa's version. Groups prompts by length to avoid padding issue"""
-    sents, sorted_idxs = zip(*sorted(zip(sents, range(len(sents))), key=lambda x: len(x[0])))
-    minibatches, mb2linenos = [], []
-    curr_batch = []
-    curr_linenos = []
-    for i in range(len(sents)):
-        if len(curr_batch) == bsz:
-            minibatches.append(curr_batch)
-            mb2linenos.append(curr_linenos)
-
-            curr_batch = [sents[i]]
-            curr_linenos = [sorted_idxs[i]]
-        else:
-            curr_batch.append(sents[i])
-            curr_linenos.append(sorted_idxs[i])
-    return minibatches, mb2linenos
-
-
 def batchify(prompts, batch_size):
+    """
+    Batch prompts
+    """
     # Edge case: 1 prompt per batch
     if len(prompts) == batch_size:
         return [[p] for p in prompts], [[i] for i in range(len(prompts))]
@@ -72,15 +56,21 @@ def batchify(prompts, batch_size):
 
 
 def parse_args():
+    """Process commandline arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--prompt-path", type=str, default="/home/aadelucia/gpt/writing_prompts/test.wp.src")
-    parser.add_argument("--output-path", type=str, default="output.txt")
-    parser.add_argument("--model-name-or-path", type=str, default=None, help="path to our model")
-    parser.add_argument("--length", type=int, default=200)
-    parser.add_argument("--bsz", type=int, default=20)
+    parser.add_argument("--prompt-path", type=str, default="/home/aadelucia/gpt/writing_prompts/test.wp.src",
+        help="Path to prompts, delineated by newlines")
+    parser.add_argument("--output-path", type=str, default="output.csv", 
+        help="Output path for generated repsonses in CSV format with columns <id>,<prompt>,<response>")
+    parser.add_argument("--model-name-or-path", type=str, default=None, help="Path to model")
+    parser.add_argument("--top-p", type=float, nargs="+", default=[0.0, 0.3, 0.5, 0.7, 0.9, 0.95, 1.0], 
+        help="Top-p (nucleus sampling) values to test. Can pass more than one value. Default values were used in the paper.")
+    parser.add_argument("--length", type=int, default=200, help="Maximum length of response (not including prompt length)")
+    parser.add_argument("--bsz", type=int, default=20, help="Batch size")
+    parser.add_argument("--display-progress", type=int, default=100, help="How often to print the generation progress")
     
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-    parser.add_argument("--no-cuda", action="store_true", help="Avoid using CUDA when available")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed for initialization")
+    parser.add_argument("--no-cuda", action="store_true", help="Avoid using CUDA even if available")
 
     args = parser.parse_args()
     args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
@@ -91,7 +81,7 @@ if __name__ == "__main__":
     # Load training parameters
     args = parse_args()
 
-    # Claim the GPU
+    # Claim the GPU (research cluster-specific issue)
     try:
         torch.ones(1).to(args.device)
     except RuntimeError as err:
@@ -105,12 +95,11 @@ if __name__ == "__main__":
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"  # Hack to be able to batch generate
 
+    # Load and batch the prompts for quicker generation
     logging.info(f"Loading prompts from {args.prompt_path}")
     prompt_lst = []
     with open(args.prompt_path, 'r') as f:
         prompt_lst = [f"{line.strip()} [RESPONSE]" for line in f.readlines()]
-
-    # Batch the prompts for quicker generation
     logging.info("total number of sentences = {}".format(len(prompt_lst)))
     prompt_lst_batch, prompt_lst_batch_idx = batchify(prompt_lst, args.bsz)
     logging.info("total batch size = {}".format(len(prompt_lst_batch)))
@@ -122,7 +111,7 @@ if __name__ == "__main__":
 
     for i, (prompts, prompts_idx) in enumerate(zip(prompt_lst_batch, prompt_lst_batch_idx)):
         # Log progress
-        if i % 100 == 0:
+        if i % args.display_progress == 0:
             logging.info(f"On batch {i} out of {len(prompt_lst_batch)}")
     
         # Encode prompts
@@ -131,7 +120,7 @@ if __name__ == "__main__":
         encoded_mask = encoded_prompt_dict['attention_mask'].to(args.device)
         end_of_prompt_idx = len(encoded_prompt[0])
 
-        for p in [0.0, 0.3, 0.5, 0.7, 0.9, 0.95, 1.0]:
+        for p in args.top_p:
             # Greedy decoding
             if p == 0.0:
                 output_sequences = model.generate(
